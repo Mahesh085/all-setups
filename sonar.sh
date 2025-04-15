5b3af4246c63df4b098439484b8cb8dafd0e5e76
Mahesh085 <69253765+Mahesh085@users.noreply.github.com>
2025-04-16 01:16:11 +0530

    Updated the Script without any manual steps.

--- sonar.sh
+++ sonar.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 #Launch an instance with 9000 and t2.medium
-cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
-unzip sonarqube-8.9.6.50800.zip
+set -x
+#Script is here
+wget -O /opt/sonarqube.zip https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
+unzip /opt/sonarqube.zip -d /opt/
 amazon-linux-extras install java-openjdk11 -y
 useradd sonar
-chown sonar:sonar sonarqube-8.9.6.50800 -R
-chmod 777 sonarqube-8.9.6.50800 -R
-su - sonar
-
-#run this on server manually
-#sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start
-#echo "user=admin & password=admin"
+chown sonar:sonar /opt/sonarqube-8.9.6.50800 -R
+chmod 777 /opt/sonarqube-8.9.6.50800 -R
+#Deleting the .ZIP file as it is extracted
+rm -rf /opt/sonarqube.zip
+#Starting the SonarQube via Sonar User
+su - sonar -c "/opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start"
