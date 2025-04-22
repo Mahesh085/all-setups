8ccf7fc44b23e61b6a082b247d6124caee42b87c
Uma Mahesh <69253765+Mahesh085@users.noreply.github.com>
2025-04-22 09:34:08 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ yum install git java-1.8.0-openjdk maven -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-#STEP-3: DOWNLOAD JAVA11 AND JENKINS
+#STEP-3: DOWNLOAD JAVA-17 AND JENKINS
 yum install java-17-amazon-corretto -y
 yum install jenkins -y
 update-alternatives --config java
