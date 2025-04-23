8bd524e6af284004cc162758031e225ad5f68278
Uma Mahesh <69253765+Mahesh085@users.noreply.github.com>
2025-04-24 00:06:35 +0530

    Updated the Script with Colors

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,30 @@
+#!/bib/bash
+set -x
+set -e
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+Normal='\033[0m'
+trap 'echo -e "${RED}"Error at $LINENO"' ERR
 #STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
 yum install git java-1.8.0-openjdk maven -y
 
+echo -e "${GREEN}Installed GIT, Java 1.8.0 and Maven Successfully${Normal}"
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+echo -e "${GREEN} Successfully Downloaded teh REPO Files from jenkins.io${Normal}"
 
 #STEP-3: DOWNLOAD JAVA-17 AND JENKINS
 yum install java-17-amazon-corretto -y
+echo -e "${GREEN} Installed Java 17 for Jenkins${Normal}"
 yum install jenkins -y
+
+echo -e "${GREEN} Installed Jenkins Successfully${Normal}"
+
 update-alternatives --config java
+echo -e "${GREEN}Updating the Jenkins${Normal}"
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins.service
+echo -e "${GREEN}Started Jenkins Successfully${Normal}"
 systemctl status jenkins.service
